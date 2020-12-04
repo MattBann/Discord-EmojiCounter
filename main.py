faf9c47fdae3648389d726feff7c60f41e217c97
Matt <69541270+MattBann@users.noreply.github.com>
2020-12-04 18:20:23 +0000

    Improved main.py readability

--- main.py
+++ main.py
@@ -5,9 +5,9 @@ import databaseConnection
 
 
 class MyClient(discord.Client):
-    initialised = False
     keyword = 'counter'
 
+    # List of valid emojis (to be moved into seperate file/database for ease of change)
     validEmojis = (':zeepledome:', ':william:', ':tories:', ':suvek:', ':subscribe:', ':stressfolky:', ':stephen:',
                    ':smoljoe:', ':simp:', ':shrek:', ':shegan:', ':schlattpog:', ':rj:', ':pogchamp:', ':pepecross:',
                    ':parkourkidpog:', ':nathan:', ':morganfreewoman:', ':mehtab:', ':matthew:', ':longalex:', ':like:',
@@ -18,6 +18,7 @@ class MyClient(discord.Client):
 
     countList = []
 
+    # On start connect to database and load data
     def __init__(self,path):
         super().__init__()
         self.database = databaseConnection.Database(
@@ -27,34 +28,28 @@ class MyClient(discord.Client):
     def load_data(self):
         self.countList = self.database.load_data()
 
-
+    # Disord event when it has logged in
     async def on_ready(self):
         print('Logged on as {0}!'.format(self.user))
 
+    # Save contents of count to the database and reload data
     def save_data(self):
         for i in self.countList:
             if i.id is None:
                 self.database.add_data(i.author, i.channel, i.time, i.emoji, i.server)
         self.load_data()
 
-
+    # Discord event when a message is sent from a channel in a guild it is in
     async def on_message(self, message):
         print('Message from {0.author}: {0.content}'.format(message))
 
+        # Ignore own messages
         if message.author == self.user:
             return
 
-        # for role in message.author.roles:
-        #     print(role)
-        # if message.content == "init" and self.initialised is False:
-        #     await message.channel.send("Counter tied to this channel")
-        #     self.initialised = True
-        #     self.channelId = message.channel.id
-        #     print(self.channelId)
-
-        if self.initialised is False and message.content.startswith('counter init'):
+        # Initialisation (note always uses 'counter' keyword)
+        if message.content.startswith('counter keyword'):
             self.keyword = message.content.split(' ')[2]
-            self.initialised = True
             await message.channel.send('Set new keyword')
 
         # Bot commands
@@ -64,9 +59,13 @@ class MyClient(discord.Client):
             itemCount = 0
 
             if commands[0] == 'count':
+                
+                # Check the emoji to find is valid and get its simplified form
                 for i in self.validEmojis:
                     if i in commands[1]:
                         emojiToFind = i
+
+                # For simple emoji counts:
                 if len(commands) == 2:
                     for i in self.countList:
                         if i.emoji == emojiToFind and i.server == str(message.guild):
@@ -74,7 +73,10 @@ class MyClient(discord.Client):
                     await message.channel.send(commands[1] + ' was used ' + str(itemCount) + ' times')
                     return
 
+                # For more complex commands:
                 elif len(commands) == 3:
+
+                    # User specific command
                     if '@' in commands[2]:
                         for i in self.countList:
                             if ((i.emoji == emojiToFind) and (str(i.author) in commands[2])) and i.server == str(message.guild):
@@ -82,7 +84,8 @@ class MyClient(discord.Client):
                         await message.channel.send(
                             commands[1] + ' was used ' + str(itemCount) + ' times by ' + str(commands[2]))
                         return
-
+                    
+                    # Channel specific command:
                     else:
                         print(commands[2])
                         for i in self.countList:
@@ -92,12 +95,10 @@ class MyClient(discord.Client):
                             commands[1] + ' was used ' + str(itemCount) + ' times in ' + str(commands[2]))
                         return
 
+            # Save command and confirmation message
             elif commands[0] == 'save':
                 self.save_data()
                 await message.channel.send('Data saved and reloaded')
-
-        elif message.content == '!event':
-            await message.channel.send('Something that Will is gonna do idk')
 
         # Emoji counter:
         if ':' in message.content:
@@ -108,6 +109,7 @@ class MyClient(discord.Client):
                             None, message.author.id, message.channel.id, str(time.localtime()), str(i), str(message.guild)))
 
 
+# Get discord secret and database path
 file = open('DiscordUserSecret.txt','r')
 UserSecret = file.read()
 file.close()
